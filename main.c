fb38b6e88af1044f5cb613bc469289809452a4bc
Kevin Hilman <khilman@ti.com>
2011-03-18 10:01:51 -0700

    Revert "only stop xfers that were started"
    This reverts commit 1dc0473918a4488c7d69bf4ce9cd8d683ee64726.
    
    Conflicts:
    
    	main.c

--- main.c
+++ main.c
@@ -44,7 +44,6 @@ struct dma_test_s {
 	unsigned int src_buf_phys, dest_buf_phys;
 	unsigned int count, good;
 	unsigned int max_transfers;
-	bool started;
 };
 static struct dma_test_s dma_test[MAX_CHANNELS];
 
@@ -71,13 +70,11 @@ static void dma_callback(int lch, u16 ch_status, void *data) {
 		t->count++;
 
 		if (debug)
-			printk("dma_callback(): lch=0x%x, ch_status=0x%04x, count=%d/%d, started=%d\n",
-			       lch, ch_status, t->count, t->max_transfers, t->started);
+			printk("dma_callback(): lch=0x%x, ch_status=0x%04x, count=%d/%d\n",
+			       lch, ch_status, t->count, t->max_transfers);
 
-		if (t->started && 
-		    t->max_transfers && (t->count >= t->max_transfers)) {
+		if (t->max_transfers && (t->count >= t->max_transfers)) {
 			omap_stop_dma(t->dma_ch);
-			t->started = false;
 		}
 	} else {
 		printk("dma_callback(): Unexpected event on channel %d\n", 
@@ -124,7 +121,6 @@ static void __exit dmatest_cleanup(void)
 	for(i=0; i<channels; i++) {
 		if (dma_test[i].dma_ch >= 0) {
 			omap_stop_dma(dma_test[i].dma_ch);
-			dma_test[i].started = false;
 
 			if (dma_test[i].next_ch != -1)
 				omap_dma_unlink_lch(dma_test[i].dma_ch,
@@ -160,13 +156,11 @@ static int dmatest_start(void) {
 			printk("   Start DMA channel %d\n", 
 			       dma_test[0].dma_ch);
 		omap_start_dma(dma_test[0].dma_ch);
-		dma_test[0].started = true;
 	} else for(i=0; i<channels; i++) {
 		if (debug) 
 			printk("   Start DMA channel %d\n", 
 			       dma_test[i].dma_ch);
 		omap_start_dma(dma_test[i].dma_ch);
-		dma_test[i].started = true;
 	}
 
 	return 0;
